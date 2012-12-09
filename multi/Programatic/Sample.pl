4e09015aa3d34c70debbe463a94e0ed486da59d3
Geoffrey <Geoffrey@Jcmbsoft.com>
2012-12-09 09:07:42 -0700

    Simple Python Application for Programatic

--- multi/Programatic/Sample.pl
+++ multi/Programatic/Sample.pl
@@ -2,6 +2,10 @@
 use strict;
 use LWP::Simple;
 
+if (ARGC == 1) {
+   print "Programatic Sample Application"
+   }
+
 print get( "http://192.168.253.49/prog/show?SystemName" ) ;
 print get( "http://192.168.253.49/prog/show?gpstime" ) ;
 print get( "http://192.168.253.49/prog/show?position" ) ;
