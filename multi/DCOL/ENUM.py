34350e6dfbc03cfe121e673d9763e6ccb20d7e8e
jcmb <GeoffreyKirk@Gmail.com>
2013-02-10 07:36:40 -0700

    Enum class

---
+++ multi/DCOL/ENUM.py
@@ -0,0 +1,10 @@
+#ENUM
+#This came from a stack overflow post
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.iteritems())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+
