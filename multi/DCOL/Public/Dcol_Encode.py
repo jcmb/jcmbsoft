67e37001299dfc3eab564d594220a41ea84d1d83
jcmb <GeoffreyKirk@Gmail.com>
2013-03-11 12:08:25 -0600

    Changed how we build the packet, fixed a bug that ment it didn't work
    also some commented out debugs

--- multi/DCOL/Public/Dcol_Encode.py
+++ multi/DCOL/Public/Dcol_Encode.py
@@ -2,16 +2,25 @@
 # A simple class for encoding DCOL packet.
 
 from DCOL_Decls import *
+from binascii import hexlify
 
 class Dcol_Encode:
     def encode (self, packet_Id, data) :
         packet=bytearray()
+#        print "packet start"
+#        print hexlify(packet)
         packet.append(STX)
+#        print "packet stx"
+#        print hexlify(packet)
         packet.append(0) # Status Byte
         packet.append(packet_Id)
         packet.append(len(data))
+
         if len(data):
-            packet.append(data)
+            for b in data :
+                packet.append(b)
+#                print "packet"
+#                print hexlify(packet)
 
         Checksum = 0;
         for i  in range (TrimComm_First_Checksum_Location,len(packet)):
@@ -20,5 +29,8 @@ class Dcol_Encode:
         Checksum = Checksum % 256
         packet.append(Checksum)
         packet.append(ETX)
+#        print "packet"
+#        print hexlify(packet)
 
         return packet
+
